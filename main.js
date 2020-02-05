31da4a1d6c4e9a467119f6d352dbb5123a057dad
Justin Corbin <jcorbin2290@gmail.com>
2020-02-05 15:10:16 -0700

    toggle effect for buttons finalized in js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var studyButton = document.querySelector('.study-btn');
 var meditateButton = document.querySelector('.meditate-btn');
-var excerciseButton = document.querySelector('.excercise-btn');
+var excerciseButton = document.querySelector('.exercise-btn');
 var buttonBox = document.querySelector('.button-box');
 var zeroStateBtnArray = [studyButton, meditateButton, excerciseButton];
 
@@ -9,25 +9,25 @@ buttonBox.addEventListener('click', function() {
   toggleButton(event);
 });
 
-function toggleButton(event) {
-  var currentCategory = null;
-  if(event.target.classList.contains('category-btn')) {
-    currentCategory = event.target.dataset.category;
-    event.target.classList.add('active');
-    var buttonImg = event.target.firstElementChild;
-    buttonImg.src = `assets/${currentCategory}-active.svg` ;
-  // } else if(event.target.classList.contains('active')){
-  //   // currentCategory = event.target.dataset.category;
-  //   // removeActiveClass(currentCategory);
-  //   console.log(1);
-  // }
-}
-
 function removeActiveClass(currentCategory) {
   for(var i = 0; i < zeroStateBtnArray.length; i++) {
-    zeroStateBtnArray[i].classList.remove('active');
+    if(zeroStateBtnArray[i].dataset.category !== currentCategory) {
+			zeroStateBtnArray[i].classList.remove('active');
+			zeroStateBtnArray[i].firstElementChild.src = `assets/${zeroStateBtnArray[i].dataset.category}.svg`;
+		}
   }
 }
 
-// if target doesnt contain class of hidden, add hidden
-// target brother'element toggle delete classlist of hidden;
+function toggleButton(event) {
+	var currentCategory = event.target.dataset.category;
+	var buttonImg = event.target.firstElementChild;
+	var classList = event.target.classList;
+  if(classList.contains('active')) {
+		classList.remove('active');
+		buttonImg.src = `assets/${currentCategory}.svg`;
+	} else {
+    classList.add('active');
+		buttonImg.src = `assets/${currentCategory}-active.svg`;
+	}
+	removeActiveClass(currentCategory)
+}
