fd53d54c23c3f696df465a17546adb68020c4e44
Brian Roxbury <roxburybrian@gmail.com>
2020-02-06 15:03:07 -0700

    add alert for no imput

--- main.js
+++ main.js
@@ -3,11 +3,35 @@ var meditateButton = document.querySelector('.meditate-btn');
 var excerciseButton = document.querySelector('.exercise-btn');
 var buttonBox = document.querySelector('.button-box');
 var zeroStateBtnArray = [studyButton, meditateButton, excerciseButton];
+var zeroStateWarningBtn = document.getElementById('no-input-alert');
+var accomplishInput = document.querySelector('.accomplish-input');
+var minutesInput = document.querySelector('.minutes-input');
+var secondsInput = document.querySelector('.seconds-input');
+var startBtn = document.querySelector('.start-btn');
+var inputArray = [accomplishInput, minutesInput, secondsInput];
 
+
+startBtn.addEventListener('click', inputAlert)
 
 buttonBox.addEventListener('click', function() {
   toggleButton(event);
 });
+
+
+function inputAlert() {
+  var isNotSelected = null;
+  for(var i = 0; i < zeroStateBtnArray.length; i++) {
+    if(!zeroStateBtnArray[i].classList.contains("active")) {
+        isNotSelected = true;
+    }
+  }
+   for(var i = 0; i < inputArray.length; i++) {
+     if(inputArray[i].value === '' || isNotSelected ) {
+       //empty input field OR !== class of .active then display alert below:
+     zeroStateWarningBtn.classList.remove('toggle-alert');
+     }
+   }
+}
 
 function removeActiveClass(currentCategory) {
   for(var i = 0; i < zeroStateBtnArray.length; i++) {
