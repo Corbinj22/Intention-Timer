668990d414bc0ac3642da9389140665765c1dcfd
Justin Corbin <jcorbin2290@gmail.com>
2020-02-11 17:24:24 -0700

    fixed js to resemble rubric

--- main.js
+++ main.js
@@ -7,14 +7,11 @@ var minutesInput = document.querySelector('.minutes-input');
 var secondsInput = document.querySelector('.seconds-input');
 var sumbitButton = document.querySelector('.start-btn');
 var leftColumn = document.querySelector('.column-left');
-var zeroStateWarningBtn = document.querySelector('.no-input-alert');
+var zeroStateWarningButton = document.querySelector('.no-input-alert');
 var accomplishInput = document.querySelector('.accomplish-input');
 var minutesInput = document.querySelector('.minutes-input');
 var secondsInput = document.querySelector('.seconds-input');
-var outputAccomplisment = document.querySelector('.users-activity-choice');
 var timeStartButton = document.querySelector('.time-button');
-var logBtn = document.querySelector('.log-activity');
-var counter = 0;
 
 
 var zeroStateBtnArray = [studyButton, meditateButton, exerciseButton];
@@ -30,22 +27,22 @@ leftColumn.addEventListener('click', function() {
 function checkInput(event) {
   minutesInput.value = minutesInput.value.replace(/[^0-9]/, '');
   secondsInput.value = secondsInput.value.replace(/[^0-9]/, '');
-}
+};
 
 function removeActiveClass(currentCategory) {
   for(var i = 0; i < zeroStateBtnArray.length; i++) {
-    if(zeroStateBtnArray[i].dataset.category !== currentCategory) {
+    if (zeroStateBtnArray[i].dataset.category !== currentCategory) {
 			zeroStateBtnArray[i].classList.remove('active');
 			zeroStateBtnArray[i].firstElementChild.src = `assets/${zeroStateBtnArray[i].dataset.category}.svg`;
 		}
   }
-}
+};
 
 function toggleButton(event) {
 	var currentCategory = event.target.dataset.category;
 	var buttonImg = event.target.firstElementChild;
 	var classList = event.target.classList;
-  if(classList.contains('active')) {
+  if (classList.contains('active')) {
 		classList.remove('active');
 		buttonImg.src = `assets/${currentCategory}.svg`;
 	} else {
@@ -53,76 +50,82 @@ function toggleButton(event) {
 		buttonImg.src = `assets/${currentCategory}-active.svg`;
 	}
 	removeActiveClass(currentCategory)
-}
+};
 
 function inputAlert() {
   var isSelected = null;
   var filledInput = true;
   for(var i = 0; i < zeroStateBtnArray.length; i++) {
-    if(zeroStateBtnArray[i].classList.contains("active")) {
-    isSelected = true;
+    if (zeroStateBtnArray[i].classList.contains("active")) {
+      isSelected = true;
   }
-}
+};
+
   for(var i = 0; i < inputArray.length; i++) {
-    if((inputArray[i].value === '') || !isSelected) {
-      zeroStateWarningBtn.classList.remove('toggle-alert');
+    if ((inputArray[i].value === '') || !isSelected) {
+      zeroStateWarningButton.classList.remove('toggle-alert');
       filledInput = false;
     }
+  };
+
+    if (filledInput && isSelected) {
+      changeLeftBox();
   }
-    if(filledInput && isSelected) {
-    changeLeftBox();
-  }
-}
+};
 
 function changeLeftBox() {
   var activityChoice = accomplishInput.value;
   var minutesTime = minutesInput.value;
   var secondsTime = secondsInput.value;
-  if(secondsInput.value < 10) {
-    secondsTime = `0${secondsTime}`;
-  }
+  if (secondsInput.value < 10) {
+    secondsTime = `0${secondsTime}`
+  };
+
   leftColumn.innerHTML = "";
   leftColumn.insertAdjacentHTML('afterbegin',`
-  <h3>Current Activity</h3>
-  <div class="user-activity-box">
-    <div class="inserted-html">
-      <p class="users-activity-choice">${activityChoice}</p>
-      <p class ="timer">${minutesTime}:${secondsTime}</p>
-      <button class="time-button" type="button" name="button">Start</button>
-      <button class="log-activity hidden"type="button">Log Activity</button>
-    </div>
-  </div>`);
+    <h3>Current Activity</h3>
+    <div class="user-activity-box">
+      <div class="inserted-html">
+        <p class="users-activity-choice">${activityChoice}</p>
+        <p class ="timer">${minutesTime}:${secondsTime}</p>
+        <button class="time-button" type="button" name="button">Start</button>
+        <button class="log-activity hidden"type="button">Log Activity</button>
+      </div>
+    </div>`);
+
   for(var i = 0; i < zeroStateBtnArray.length; i++) {
-    if(zeroStateBtnArray[i].classList.contains('active')) {
+    if (zeroStateBtnArray[i].classList.contains('active')) {
       timeStartButton = document.querySelector('.time-button');
       timeStartButton.classList.add(`time-button-${zeroStateBtnArray[i].dataset.category}`);
     }
   }
-}
+};
 
 function timerStart() {
-  if(event.target.classList.contains('time-button')) {
-  document.querySelector('.time-button').disabled = true;
-  var seconds = parseInt(secondsInput.value);
-  var minutes = parseInt(minutesInput.value);
-  var totalTime = (minutes*60) + seconds;
-  var timer = document.querySelector('.timer');
-  var logBtn = document.querySelector('.log-activity');
-  var timeTracker = setInterval(function () {
-    counter++;
-    var s = totalTime - counter;
-    var min = Math.floor(s / 60);
-    var sec = s % 60;
-    timer.innerHTML = `${min}:${sec}`;
-    if(sec < 10) {
-      timer.innerHTML = `${min}:0${sec}`;
-    }
-    if(s === 0) {
-      clearInterval(timeTracker);
-      logBtn.classList.remove('hidden');
-      timeStartButton.innerHTML = `${"COMPLETE!"}`;
+  if (event.target.classList.contains('time-button')) {
+    document.querySelector('.time-button').disabled = true;
+    var counter = 0
+    var seconds = parseInt(secondsInput.value);
+    var minutes = parseInt(minutesInput.value);
+    var totalTime = (minutes*60) + seconds;
+    var timer = document.querySelector('.timer');
+    var logBtn = document.querySelector('.log-activity');
+    var timeTracker = setInterval(function() {
+      counter++;
+      var s = totalTime - counter;
+      var min = Math.floor(s / 60);
+      var sec = s % 60;
+      timer.innerHTML = `${min}:${sec}`;
 
-    }
-  }, 1000);
-}
-}
+      if (sec < 10) {
+        timer.innerHTML = `${min}:0${sec}`;
+      };
+
+      if (s === 0) {
+        clearInterval(timeTracker);
+        logBtn.classList.remove('hidden');
+        timeStartButton.innerHTML = `${"COMPLETE!"}`;
+      }
+    }, 1000);
+  }
+};
